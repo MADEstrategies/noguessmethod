a49bd232f7d8ed23d128bfd5b895403109a88418
NOGUESSMETHOD <noguessmethod@gmail.com>
2026-05-29 22:21:33 -0400

    Use environment variables for Supabase client

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://lbchkywhvpcutwjksdno.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiY2hreXdodnBjdXR3amtzZG5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDMyMjksImV4cCI6MjA5MzY3OTIyOX0.Zj_2evSTsVC1IpAMYp-6DPMzuIHtna7_q0z5SX7k_e8'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
 
