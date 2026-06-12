edf0d2372ca65adff7d99ab6c355db76a3d2cf3f
NOGUESSMETHOD <noguessmethod@gmail.com>
2026-06-12 09:01:01 -0400

    Reorganize routes and remove Free component

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import Admin from './pages/Admin'
 import Settings from './pages/Settings'
 import Upgrade from './pages/Upgrade'
 import Success from './pages/Success'
-import Free from './pages/Free'
 import Investors from './pages/Investors'
 import NotFound from './pages/NotFound'
 import Checkout from './pages/Checkout'
@@ -28,24 +27,21 @@ function AnimatedRoutes() {
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-
-        
-        <Route path='/courses'  element={<ProtectedRoute><Course /></ProtectedRoute>} />
-        <Route path='/store'    element={<ProtectedRoute><Store /></ProtectedRoute>} />
-        <Route path='/library'  element={<ProtectedRoute><Library /></ProtectedRoute>} />
-        <Route path='/workouts' element={<ProtectedRoute><Workouts /></ProtectedRoute>} />
         <Route path="/"          element={<Home />} />
         <Route path="/login"     element={<Login />} />
         <Route path="/signup"    element={<Signup />} />
-        <Route path="/free"      element={<Free />} />
         <Route path="/upgrade"   element={<Upgrade />} />
         <Route path="/investors" element={<Investors />} />
+        <Route path="/courses"   element={<ProtectedRoute><Course /></ProtectedRoute>} />
+        <Route path="/store"     element={<ProtectedRoute><Store /></ProtectedRoute>} />
+        <Route path="/library"   element={<ProtectedRoute><Library /></ProtectedRoute>} />
+        <Route path="/workouts"  element={<ProtectedRoute><Workouts /></ProtectedRoute>} />
         <Route path="/account"   element={<ProtectedRoute><Account /></ProtectedRoute>} />
         <Route path="/workout"   element={<ProtectedRoute><Workout /></ProtectedRoute>} />
         <Route path="/settings"  element={<ProtectedRoute><Settings /></ProtectedRoute>} />
         <Route path="/macros"    element={<ProtectedRoute><Macros /></ProtectedRoute>} />
         <Route path="/checkout"  element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-        <Route path="/cancel" element={<ProtectedRoute><Cancel /></ProtectedRoute>} />
+        <Route path="/cancel"    element={<ProtectedRoute><Cancel /></ProtectedRoute>} />
         <Route path="/success"   element={<ProtectedRoute><PaidRoute><Success /></PaidRoute></ProtectedRoute>} />
         <Route path="/admin"     element={<ProtectedRoute><Admin /></ProtectedRoute>} />
         <Route path="*"          element={<NotFound />} />
