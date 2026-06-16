271cfd1aec5807145536d0da4c3de580b1a1f66a
NOGUESSMETHOD <noguessmethod@gmail.com>
2026-06-15 23:38:54 -0400

    Update Footer links to include Blog

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
+
 export default function Footer() {
   return (
     <footer className="footer">
       <span>NoGuessMethod</span>
       <span>
-        <Link to="/">Main</Link> · <Link to="/upgrade">Pricing</Link> · <Link to="/investors">Investors</Link>
+        <Link to="/">Main</Link> · <Link to="/blog">Blog</Link> · <Link to="/upgrade">Pricing</Link> · <Link to="/investors">Investors</Link>
       </span>
       <span>© 2026 NoGuessMethod</span>
     </footer>
